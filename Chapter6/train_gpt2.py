c03b1868613cbcdf7a410694ecc15d95399dfef6
Mohammed Albrayh <83492447+MohammmedAb@users.noreply.github.com>
2024-06-12 20:20:07 +0300

    gpt nn.Module

---
+++ Chapter6/train_gpt2.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+import torch
+import torch as nn
+from torch.nn import functional as F
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        self.ln_1 = nn.LayerNorm(config.n_embd) # layernorm 1
+        self.attn = CasualSelfAttention(config) # multi-head self-attention
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config) # feedforward neural network
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+        
+
+@dataclass
+class GPTConfig:
+    block_size: int= 256
+    vocab_size: int = 65
+    n_layers: int = 6
+    n_head: int = 6
+    n_embd: int = 384
+
+class GPT:
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd), # token embedding
+            wpe = nn.Embedding(config.block_size, config.n_embd), # positional embedding
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layers)]), # transformer block (hidden layers)
+            ln_f = nn.LayerNorm(config.n_embd) # layernorm after the last block
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # language model head
+
