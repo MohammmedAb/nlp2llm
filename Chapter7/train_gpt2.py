4e4c020f200a44277ce6d2e691fba473b18c42d4
Mohammed Albrayh <83492447+MohammmedAb@users.noreply.github.com>
2024-06-13 16:28:20 +0300

    Add code for generating text using GPT2 language model

--- Chapter7/train_gpt2.py
+++ Chapter7/train_gpt2.py
@@ -151,5 +151,40 @@ class GPT(nn.Module):
 
         return model
 
+num_return_sequences = 5
+max_length = 30
 model = GPT.from_pretrained('gpt2')
-print("didn't crash!")
+
+model.eval()
+# model.to('cuda')
+
+#prefix tokens as starting point for the model
+import tiktoken
+enc = tiktoken.get_encoding('gpt2')
+tokens = enc.encode("Hello i'm a language model, ")
+tokens = torch.tensor(tokens, dtype=torch.long)
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+x = tokens
+# x = tokens.to('cuda')
+
+# generate some text
+torch.manual_seed(42)
+# torch.cuda.manual_seed(42)
+while x.size(1) < max_length:
+
+    with torch.no_grad():
+        logits = model(x)
+        logits = logits[:, -1, :] # take the logits at the last position
+
+        probs = F.softmax(logits, dim=-1)
+        topk_probs, topk_indices = torch.topk(probs, 10, dim=-1)
+
+        ix = torch.multinomial(topk_probs, 1) # sample from the top-k probabilities
+
+        xcol = torch.gather(topk_indices, -1, ix) 
+        x = torch.cat((x, xcol), dim=1)
+
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print('>',decoded)
